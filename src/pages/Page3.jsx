b446d8d7d772e3a6dc0f0c4c00e6e20ed4ef6297
BangDen24 <farrelkusumah24@gmail.com>
2024-08-28 15:43:22 +0700

    we almost there

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -13,30 +13,136 @@ import e from "../assets/55.jpg";
 
 const Page3 = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const slides = [a, b, c, d, e];
 
   const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    if (isAnimating) return;
+    const nextSlideIndex = (currentSlide + 1) % slides.length;
+    navigate(1, currentSlide, nextSlideIndex);
+    setCurrentSlide(nextSlideIndex);
   };
 
   const prevSlide = () => {
-    setCurrentSlide((prevSlide) =>
-      prevSlide === 0 ? slides.length - 1 : prevSlide - 1
+    if (isAnimating) return;
+    const prevSlideIndex =
+      currentSlide === 0 ? slides.length - 1 : currentSlide - 1;
+    navigate(-1, currentSlide, prevSlideIndex);
+    setCurrentSlide(prevSlideIndex);
+  };
+
+  const navigate = (direction, current, next) => {
+    setIsAnimating(true);
+
+    const currentSlideElem = document.querySelector(`.slide-${current}`);
+    const nextSlideElem = document.querySelector(`.slide-${next}`);
+
+    gsap.set(nextSlideElem, { zIndex: 99 });
+
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 0.8,
+        ease: "power4.inOut",
+      },
+      onStart: () => {
+        //ADD GSAP INTRO NYA DISINI
+        nextSlideElem.classList.add("slide--current");
+      },
+      onComplete: () => {
+        currentSlideElem.classList.remove("slide--current");
+        gsap.set(nextSlideElem, { zIndex: 1 });
+        setIsAnimating(false);
+      },
+    });
+
+    tl
+    .addLabel('start', 0);
+
+    tl.to(
+      currentSlideElem,
+      {
+        scale: 0.6, // [HIGHLIGHT] Slide lama mengecil
+        autoAlpha: 0, // [HIGHLIGHT] Slide lama menjadi tidak terlihat
+        onComplete: () => gsap.set(currentSlideElem, { scale: 1, autoAlpha: 1 }), // [HIGHLIGHT] Reset scale dan opacity
+      },
+      "start"
+    );
+
+    document.querySelectorAll(".deco").forEach((deco, pos, arr) => {
+      const reverseDeco = arr[arr.length - 1 - pos];
+
+      tl.fromTo(
+        reverseDeco, {
+          xPercent: pos % 2 === 1 ? - 100 : 100,
+          autoAlpha : 1,
+        },
+        {
+          xPercent: pos % 2 === 1 ? -50 : 50,
+          onComplete: () => {
+            if (pos === arr.length - 1){
+              currentSlideElem.classList.remove("slide--current");
+              nextSlideElem.classList.add("slide--current");
+            }
+          },
+        },
+        `start+=${Math.floor((arr.length -1 -pos) / 2) * 0.14}`
+      );
+      if (!pos) {
+        tl.addLabel("middle", ">");
+      }
+    });
+    
+    tl.to(
+      currentSlideElem,
+      {
+        ease: "power4.in",
+        scale: 0.1,
+        onComplete: () => gsap.set(currentSlideElem, {scale : 1})
+      }, "start"
+    );
+    document.querySelectorAll(".deco").forEach((deco, pos, arr) => {
+      tl.to(
+        deco,
+        {
+          xPercent: pos % 2 === 1 ? -100 : 100,
+          duration : 0.5,
+        },
+        `middle+=${Math.floor(pos / 2) * 0.12}`
+      );
+    });
+  
+    // Animasi skala slide berikutnya
+    tl.fromTo(
+      nextSlideElem,
+      {
+        duration: 0.8,
+        scale: 0.6,
+        autoAlpha: 0,
+        zIndex: 0,
+      },
+      {
+        duration: 1.1,
+        ease: "expo",
+        scale: 1,
+        autoAlpha:1,
+      },
+      ">-0.8"
     );
   };
+
 
   useEffect(() => {
     // Create the Observer instance
     const observer = Observer.create({
       type: "wheel,touch,pointer",
-      onDown: () => prevSlide(), // Call prevSlide function
-      onUp: () => nextSlide(), // Call nextSlide function
+      onDown: () => prevSlide(),
+      onUp: () => nextSlide(), 
       wheelSpeed: -1,
       tolerance: 10,
     });
 
-    // Cleanup observer on component unmount
+
     return () => {
       observer.kill();
     };
@@ -61,9 +167,6 @@ const Page3 = () => {
           <Link to="/page3" className="frame__demo frame__demo--current">
             03
           </Link>
-          <Link to="/page4" className="frame__demo">
-            04
-            </Link>
         </nav>
         <nav className="slides-nav">
           <button
